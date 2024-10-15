e53d57ca812ee6a4a2205594731efba2dc4b8cb3
Benjamin <bendhallam@gmail.com>
2024-10-15 09:48:42 -0600

    fixed naming conventions

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,17 +5,15 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   const {id, username, imageSource, location, profile} = photo;
     return (
-      <div className="photo-list__item" key={id}>
+      <div className="photo-list__item">
       <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image" />
-      <div className="info">
         <div className="photo-list__user-details">
           <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
           <div className="photo-list__user-info">
-            <h3 className="username">{username}</h3>
+            <h3 className="photo-list_username">{username}</h3>
             <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
           </div>
         </div>
-      </div>
     </div>
   )
 };
