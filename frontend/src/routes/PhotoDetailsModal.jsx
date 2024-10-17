22b6e28103411aac8cf76958153faf71250f15da
Benjamin <bendhallam@gmail.com>
2024-10-17 17:53:21 -0600

    logged photo id to console

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,6 +4,7 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = ({photoId, onClose}) => {
+  console.log(photoId)
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
