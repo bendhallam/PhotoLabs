3289bbaf2a9fefc2030f8f6d926009910b9c6201
Benjamin <bendhallam@gmail.com>
2024-10-18 09:47:58 -0600

    reconfigured modal to look nicer

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -12,25 +12,31 @@ const PhotoDetailsModal = ({photo, onClose}) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
+
     <img 
       src={photo.urls.full} 
       alt={`Photo by ${photo.user.username}`} 
       className="photo-details-modal__image" 
     />
-    <div className="photo-details-modal__photographer-details">
-      <img 
-        src={photo.user.profile} 
-        alt={`${photo.user.username}'s profile`} 
-        className="photo-details-modal__photographer-profile" 
-      />
-      <div className="photo-details-modal__photographer-info">
-        <h3 className="photo-details-modal__header">{photo.user.username}</h3>
-        <p className="photo-details-modal__photographer-location">
-          {`${photo.location.city}, ${photo.location.country}`}
-        </p>
-      </div>
+
+<div className="photo-details-modal__photographer-details">
+  <img 
+    src={photo.user.profile} 
+    alt={`${photo.user.username}'s profile`} 
+    className="photo-details-modal__photographer-profile" 
+  />
+  <div className="photo-details-modal__photographer-info">
+    <h3 className="photo-details-modal__header">{photo.user.username}</h3>
+    <p className="photo-details-modal__photographer-location">
+      {`${photo.location.city}, ${photo.location.country}`}
+    </p>
+  </div>
+</div>
+    
+  <div className="photo-details-modal__images">
+      <h3 className="photo-details-modal__header">Similar Photos</h3>
       <PhotoList photos={photo.similar_photos} />
-    </div>
+      </div>
   </div>
   )
 };
