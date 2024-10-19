833a96b63227bb62ffdb110d1d9dae8ee0c9b164
Benjamin <bendhallam@gmail.com>
2024-10-19 10:19:51 -0600

    added reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+// Define the initial state
+const initialState = {
+  favouritedPhotos: [],
+  selectedPhoto: null,
+};
+
+// Define the reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_FAVOURITED_PHOTOS':
+      return {
+        ...state,
+        favouritedPhotos: action.payload,
+      };
+    case 'SET_SELECTED_PHOTO':
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+      };
+    case 'ADD_OR_REMOVE_FAVOURITE':
+      const { photoId } = action.payload;
+      const isFavourite = state.favouritedPhotos.includes(photoId);
+      return {
+        ...state,
+        favouritedPhotos: isFavourite
+          ? state.favouritedPhotos.filter((id) => id !== photoId)
+          : [...state.favouritedPhotos, photoId],
+      };
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        selectedPhoto: null,
+      };
+    default:
+      return state;
+  }
+};
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
-     favouritedPhotos: [],
-     selectedPhoto: null
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // Initialize setFavouritedPhotos
   const setFavouritedPhotos = (favouritedPhotos) => {
-    setState((prevState) => ({
-      ...prevState,
-      favouritedPhotos
-    }));
+    dispatch({ type: 'SET_FAVOURITED_PHOTOS', payload: favouritedPhotos });
   };
 
-  // Intialize setSelectedPhoto
+  // Initialize setSelectedPhoto
   const setSelectedPhoto = (selectedPhoto) => {
-    setState((prevState) => ({
-      ...prevState,
-      selectedPhoto
-    }));
+    dispatch({ type: 'SET_SELECTED_PHOTO', payload: selectedPhoto });
   };
 
   // Handle pressing heart button
-   const addOrRemoveFavourite = (photoId) => {
-     setState((prevState) => {
-       const { favouritedPhotos } = prevState;
-
-       return {
-         ...prevState,
-         favouritedPhotos: favouritedPhotos.includes(photoId)
-           ? favouritedPhotos.filter((id) => id !== photoId)
-           : [...favouritedPhotos, photoId]
-       };
-     });
-   };
-   
+  const addOrRemoveFavourite = (photoId) => {
+    dispatch({ type: 'ADD_OR_REMOVE_FAVOURITE', payload: { photoId } });
+  };
 
   // Handle clicking on a photo
   const handlePhotoSelect = (photo) => {
@@ -44,15 +63,16 @@ const useApplicationData = () => {
 
   // Handle clicking the close button
   const closeModal = () => {
-    setSelectedPhoto(null);
-  }
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
 
   return {
     state,
     addOrRemoveFavourite,
     handlePhotoSelect,
-    closeModal
-  }
-}
- 
+    closeModal,
+    setFavouritedPhotos, // Expose setFavouritedPhotos if needed
+  };
+};
+
 export default useApplicationData;
